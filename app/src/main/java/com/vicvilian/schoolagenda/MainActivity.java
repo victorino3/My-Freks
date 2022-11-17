0d26d21340d1e37540d15a86cfce1ca40244299f
victorino3 <victorinogomes1@gmail>
2022-11-17 11:59:42 +0000

    Sending setUp

---
+++ app/src/main/java/com/vicvilian/schoolagenda/MainActivity.java
@@ -0,0 +1,24 @@
+package com.vicvilian.schoolagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn_login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(getApplicationContext(),MainActivity2.class));
+            }
+        });
+    }
+}
