07f25d683eca262514053cf657721480b5762074
victorino3 <victorinogomes1@gmail>
2022-11-18 16:02:32 +0000

    Date and time already done in fragment CreateFragment.java

--- app/src/main/java/com/vicvilian/schoolagenda/ui/Create/CreateFragment.java
+++ app/src/main/java/com/vicvilian/schoolagenda/ui/Create/CreateFragment.java
@@ -1,5 +1,7 @@
 package com.vicvilian.schoolagenda.ui.Create;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +9,99 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.vicvilian.schoolagenda.R;
 
+import java.util.Calendar;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CreateFragment#newInstance} factory method to
+ * Use the {@link CreateFragment #newInstance} factory method to
  * create an instance of this fragment.
  */
 public class CreateFragment extends Fragment {
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private TextView select_Time;
+    private TextView select_Date;
+    private TextView btn_Time;
+    private TextView btn_Date;
+    private EditText title;
+    private Button btn;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public CreateFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateFragment newInstance(String param1, String param2) {
-        CreateFragment fragment = new CreateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false);
+
+        select_Time = view.findViewById(R.id.select_time);
+        title = view.findViewById(R.id.text_title);
+        select_Date = view.findViewById(R.id.select_date);
+        btn_Time = view.findViewById(R.id.button_time);
+        btn_Date = view.findViewById(R.id.button_date);
+        btn = view.findViewById(R.id.button_save_task);
+        //Setting date
+        btn_Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                System.out.println(mYear+","+mMonth+","+mDay);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                select_Date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+        //Setting time
+        btn_Time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                select_Time.setText(hourOfDay + ":" + minute);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+        });
+
+
+
+        return view;
     }
+
+
 }
